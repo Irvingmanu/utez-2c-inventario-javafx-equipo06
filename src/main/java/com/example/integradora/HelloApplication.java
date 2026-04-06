022758bfba51c9a5f869e58ff25ccb8ce7586e83
axel sanchez <20253ds084@utez.edu.mx>
2026-04-05 22:07:28 -0600

    estructura para aplicacion JavaFX creada

---
+++ src/main/java/com/example/integradora/HelloApplication.java
@@ -0,0 +1,19 @@
+package com.example.integradora;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("JavaFX-views/MainView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Aplicacion tienda de abarrotes");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
