c8645d781cc2a36dc1474bf6fd4f8c83e4d2b75a
axel sanchez <20253ds084@utez.edu.mx>
2026-04-06 20:55:34 -0600

    Merge de rama de irving a rama de axel

--- Integradora/src/main/java/com/example/integradora/repositories/FileRepository.java
+++ Integradora/src/main/java/com/example/integradora/repositories/FileRepository.java
@@ -1,4 +1,37 @@
 package com.example.integradora.repositories;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
 public class FileRepository {
+
+    private final Path pathFile = Paths.get("archivo", "productos.csv");
+
+    public void ensureFile() throws IOException {
+        if (Files.notExists(pathFile)) {
+            Files.createFile(pathFile);
+        }
+    }
+
+    public List<String> readAllLines() throws IOException {
+        ensureFile();
+        return Files.readAllLines(pathFile, StandardCharsets.UTF_8);
+    }
+
+    public void addNewLine(String line) throws IOException {
+        ensureFile();
+        Files.writeString(pathFile,
+                line + System.lineSeparator(), StandardCharsets.UTF_8, StandardOpenOption.APPEND);
+    }
+
+    public void saveFile(List<String> lines) throws IOException {
+        ensureFile();
+        Files.write(pathFile, lines, StandardCharsets.UTF_8, StandardOpenOption.TRUNCATE_EXISTING);
+    }
+
 }
