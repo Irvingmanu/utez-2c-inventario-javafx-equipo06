5e3c3bae3632dccd56d71293a00b2a75d9c98743
Irvingmanu <20253ds070@utez.edu.mx>
2026-04-06 20:28:02 -0600

    Agregando vistas al proyecto

--- Integradora/src/main/java/com/example/integradora/HelloApplication.java
+++ Integradora/src/main/java/com/example/integradora/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("views/MainView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Aplicacion tienda de abarrotes");
         stage.setScene(scene);
         stage.show();
     }
